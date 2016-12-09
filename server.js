32e818d00090843abed2212fb5b4514a23c50632
xavierouicar <xavier@ouicar.fr>
2016-12-09 17:04:43 +0100

    set password

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
 app = express();
+const basicauth = require('basicauth-middleware');
+app.use(basicauth('yes', 'ouicar'));
 app.use(express.static(path.join(__dirname, 'dist')));
 var port = process.env.PORT || 5000;
 app.listen(port);
